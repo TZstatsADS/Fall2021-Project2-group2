e52af511036ab1335dca5bfa8c9e54978c74ebaa
Kerry Cook <kerry.cook@ibm.com>
2021-10-15 17:32:01 -0400

    Add Map tab

--- app/server.R
+++ app/server.R
@@ -30,95 +30,114 @@ plan(multiprocess)
 ## build server.R
 server <- 
   function(input, output, session) {
-    ## I. Main dashboard ---------------------------------------------------
-    # 1. Value boxes  --------------------
-    violence_year <- aggregate(. ~ Report_Year,select(violence_df, -Comm_Dist_Boro), FUN=sum)
-    most_recent_totals <- violence_year %>% filter(Report_Year == max(Report_Year))
-    prev_year_totals <-  violence_year %>% filter(Report_Year == max(Report_Year)-1)
-    perc_diff <- round((most_recent_totals$totals - prev_year_totals$totals)/prev_year_totals$totals*100,2)
-    
-    # ## Create Value Boxes for Percent Difference and Totals 
-    output$PercDiff <- renderValueBox({
-      valueBox(
-        value =  paste0(perc_diff, "%" ),
-        subtitle = tags$p("YoY Percent Difference - 2019 to 2020", style = "color:black"),
-        icon = icon('export', lib = 'glyphicon'),
-        color = "green"
-      )
-    })
-    
-    output$RecentTotals <- renderValueBox({
-      valueBox(
-        value =  most_recent_totals$totals,
-        subtitle = tags$p("2020 Total Reported Cases", style = "color:black"),
-        icon = icon('export', lib = 'glyphicon'),
-        color = "blue"
-      )
-    })
-    
-    # 2. Line Charts By Borough  -------------------
-    violence_df <- read_csv('../data/family_violence_grouped.csv')
-    
-    #Counts by Year and Borough 
-    output$DVTotalCountsByYear <-renderHighchart({
-      highchart() %>%
-        hc_exporting(enabled = TRUE, formAttributes = list(target = "_blank")) %>%
-        hc_chart(type = 'line') %>%
-        hc_series( list(name = 'Manhattan', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Manhattan'], color='blue', marker = list(symbol = 'circle') ),
-                   list(name = 'Bronx', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Bronx'], color = 'green', marker = list(symbol = 'circle') ),
-                   list(name = 'Brooklyn', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Brooklyn'], color = 'pink', marker = list(symbol = 'circle') ),
-                   list(name = 'Queens', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Queens'], color = 'purple',marker = list(symbol = 'circle')  ),
-                   list(name = 'Staten Island', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Staten Island'], color = 'red',  marker = list(symbol = 'circle')  )
-        )%>%
-        hc_xAxis( categories = unique(violence_df$Report_Year) ) %>%
-        hc_yAxis( title = list(text = "Total")) %>%
-        hc_plotOptions(column = list(
-          dataLabels = list(enabled = F),
-          #stacking = "normal",
-          enableMouseTracking = T ) 
-        )%>%
-        hc_tooltip(table = TRUE,
-                   sort = TRUE,
-                   pointFormat = paste0( '<br> <span style="color:{point.color}">\u25CF</span>',
-                                         " {series.name}: {point.y}"),
-                   headerFormat = '<span style="font-size: 13px">Year {point.key}</span>'
-        ) %>%
-        hc_legend( layout = 'vertical', align = 'left', verticalAlign = 'top', floating = T, x = 100, y = 000 )
-    })
-    
-    # 2.1 Line graph Break down by Type  --------------------
-    output$DVCountsByCategory <-renderHighchart({
-      highchart() %>%
-        hc_exporting(enabled = TRUE, formAttributes = list(target = "_blank")) %>%
-        hc_chart(type = 'line') %>%
-        hc_series( #list(name = 'Domestic Violence Reports', data = violence_year$FAM_DIR, color='brown' , marker = list(enabled = F), lineWidth = 3 ),
-                   list(name = 'Felony Assaults, Family Members', data = violence_year$FAM_Fel_Assault, color = 'darkgreen', dashStyle = 'shortDot', marker = list(symbol = 'circle') ),
-                   list(name = 'Felony Assaults, Domestic Violence', data = violence_year$DV_Fel_Assault, color = 'darkblue', dashStyle = 'shortDot',  marker = list(symbol = 'triangle') ),
-                   list(name = 'Felony Rape, Family Members', data =  violence_year$FAM_Rape, color = 'darkblue', dashStyle = 'shortDot',  marker = list(symbol = 'triangle') ),
-                   list(name = 'Felony Rape, Domestic Violence', data =  violence_year$DV_Rape, color = 'purple', dashStyle = 'shortDot',  marker = list(symbol = 'triangle') )
-        )%>%
-        hc_xAxis( categories = unique(violence_df$Report_Year) ) %>%
-        hc_yAxis( title = list(text = "$ million, NZD"),
-                  plotLines = list(
-                    list(#label = list(text = "This is a plotLine"),
-                      color = "#ff0000",
-                      #dashStyle = 'shortDot',
-                      width = 2,
-                      value = 0 ) )
-        ) %>%
-        hc_plotOptions(column = list(
-          dataLabels = list(enabled = F),
-          #stacking = "normal",
-          enableMouseTracking = T )
-        )%>%
-        hc_tooltip(table = TRUE,
-                   sort = TRUE,
-                   pointFormat = paste0( '<br> <span style="color:{point.color}">\u25CF</span>',
-                                         " {series.name}: {point.y}"),
-                   headerFormat = '<span style="font-size: 13px">Year {point.key}</span>'
-        ) %>%
-        hc_legend( layout = 'vertical', align = 'left', verticalAlign = 'top', floating = T, x = 100, y = 000 )
-    })
+    ########################################################################
+    ## I. Domestic Violence dashboard --------------------------------------
+    #######################################################################
+          # 1. Value boxes  --------------------
+          violence_df <- read_csv('../data/family_violence_grouped.csv')
+          violence_year <- aggregate(. ~ Report_Year,select(violence_df, -Comm_Dist_Boro), FUN=sum)
+          most_recent_totals <- violence_year %>% filter(Report_Year == max(Report_Year))
+          prev_year_totals <-  violence_year %>% filter(Report_Year == max(Report_Year)-1)
+          perc_diff <- round((most_recent_totals$totals - prev_year_totals$totals)/prev_year_totals$totals*100,2)
+          
+          # ## Create Value Boxes for Percent Difference and Totals 
+          output$PercDiff <- renderValueBox({
+            valueBox(
+              value =  paste0(perc_diff, "%" ),
+              subtitle = tags$p("YoY Percent Difference - 2019 to 2020", style = "color:black"),
+              icon = icon('export', lib = 'glyphicon'),
+              color = "green"
+            )
+          })
+          
+          output$RecentTotals <- renderValueBox({
+            valueBox(
+              value =  most_recent_totals$totals,
+              subtitle = tags$p("2020 Total Reported Cases", style = "color:black"),
+              icon = icon('export', lib = 'glyphicon'),
+              color = "blue"
+            )
+          })
+          
+          # 2. Line Charts By Borough  ------------------
+          #Counts by Year and Borough 
+          output$DVTotalCountsByYear <-renderHighchart({
+            highchart() %>%
+              hc_exporting(enabled = TRUE, formAttributes = list(target = "_blank")) %>%
+              hc_chart(type = 'line') %>%
+              hc_series( list(name = 'Manhattan', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Manhattan'], color='blue', marker = list(symbol = 'circle') ),
+                         list(name = 'Bronx', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Bronx'], color = 'green', marker = list(symbol = 'circle') ),
+                         list(name = 'Brooklyn', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Brooklyn'], color = 'pink', marker = list(symbol = 'circle') ),
+                         list(name = 'Queens', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Queens'], color = 'purple',marker = list(symbol = 'circle')  ),
+                         list(name = 'Staten Island', data =violence_df$totals[violence_df$Comm_Dist_Boro=='Staten Island'], color = 'red',  marker = list(symbol = 'circle')  )
+              )%>%
+              hc_xAxis( categories = unique(violence_df$Report_Year) ) %>%
+              hc_yAxis( title = list(text = "Total")) %>%
+              hc_plotOptions(column = list(
+                dataLabels = list(enabled = F),
+                #stacking = "normal",
+                enableMouseTracking = T ) 
+              )%>%
+              hc_tooltip(table = TRUE,
+                         sort = TRUE,
+                         pointFormat = paste0( '<br> <span style="color:{point.color}">\u25CF</span>',
+                                               " {series.name}: {point.y}"),
+                         headerFormat = '<span style="font-size: 13px">Year {point.key}</span>'
+              ) %>%
+              hc_legend( layout = 'vertical', align = 'left', verticalAlign = 'top', floating = T, x = 100, y = 000 )
+          })
+          
+          # 2.1 Line graph Break down by Type  --------------------
+          output$DVCountsByCategory <-renderHighchart({
+            highchart() %>%
+              hc_exporting(enabled = TRUE, formAttributes = list(target = "_blank")) %>%
+              hc_chart(type = 'line') %>%
+              hc_series( #list(name = 'Domestic Violence Reports', data = violence_year$FAM_DIR, color='brown' , marker = list(enabled = F), lineWidth = 3 ),
+                         list(name = 'Felony Assaults, Family Members', data = violence_year$FAM_Fel_Assault, color = 'darkgreen', dashStyle = 'shortDot', marker = list(symbol = 'circle') ),
+                         list(name = 'Felony Assaults, Domestic Violence', data = violence_year$DV_Fel_Assault, color = 'darkblue', dashStyle = 'shortDot',  marker = list(symbol = 'triangle') ),
+                         list(name = 'Felony Rape, Family Members', data =  violence_year$FAM_Rape, color = 'darkblue', dashStyle = 'shortDot',  marker = list(symbol = 'triangle') ),
+                         list(name = 'Felony Rape, Domestic Violence', data =  violence_year$DV_Rape, color = 'purple', dashStyle = 'shortDot',  marker = list(symbol = 'triangle') )
+              )%>%
+              hc_xAxis( categories = unique(violence_df$Report_Year) ) %>%
+              hc_yAxis( title = list(text = "$ million, NZD"),
+                        plotLines = list(
+                          list(#label = list(text = "This is a plotLine"),
+                            color = "#ff0000",
+                            #dashStyle = 'shortDot',
+                            width = 2,
+                            value = 0 ) )
+              ) %>%
+              hc_plotOptions(column = list(
+                dataLabels = list(enabled = F),
+                #stacking = "normal",
+                enableMouseTracking = T )
+              )%>%
+              hc_tooltip(table = TRUE,
+                         sort = TRUE,
+                         pointFormat = paste0( '<br> <span style="color:{point.color}">\u25CF</span>',
+                                               " {series.name}: {point.y}"),
+                         headerFormat = '<span style="font-size: 13px">Year {point.key}</span>'
+              ) %>%
+              hc_legend( layout = 'vertical', align = 'left', verticalAlign = 'top', floating = T, x = 100, y = 000 )
+          })
+    ########################################################################
+    ## I. Resource Directory Map --------------------------------------
+    #######################################################################
+        resources_df <- read_csv("../data/Mayor_s_Office_to_End_Domestic_and_Gender-Based_Violence__Resource_Directory.csv")
+        output$res_map <- renderLeaflet({ 
+            leaflet(options = leafletOptions(zoomControl = FALSE),
+                    data = resources_df) %>% addTiles() %>%
+                    addMarkers(~LONGITUDE, ~LATITUDE, popup = paste(
+                      "<b>Name:</b>", resources_df$NAME, "<br>",
+                      "<b>Address:</b>", resources_df$ADDRESS, "<br>")) %>%
+              htmlwidgets::onRender(
+                "function(el, x) {
+                    L.control.zoom({ position: 'bottomright' }).addTo(this)
+                }"
+              ) %>%
+              addProviderTiles("CartoDB.Voyager") %>%
+              setView(lng = -73.935242, lat = 40.730610, zoom = 10)
+          })
   }
 
 
--- app/ui.R
+++ app/ui.R
@@ -12,7 +12,10 @@ library(DT)
 library(shinyBS)
 library(shinyjs)
 library(WDI)
-#library(geosphere)
+if (!require("leaflet")) {
+  install.packages("leaflet")
+  library(leaflet)
+}
 library(magrittr)
 library(shinycssloaders)
 options(spinner.color="#006272")
@@ -23,58 +26,6 @@ library(timevis)
 
 ## setup global variables
 maxYear_lb <- '2018'  
-
-
-## load functions
-customSentence <- function(numItems, type) {
-  paste("Feedback & suggestions")
-}
-
-customSentence_share <- function(numItems, type) {
-  paste("Love it? Share it!")
-}
-
-dropdownMenuCustom <-     function (..., type = c("messages", "notifications", "tasks"), 
-                                    badgeStatus = "primary", icon = NULL, .list = NULL, customSentence = customSentence) 
-{
-  type <- match.arg(type)
-  if (!is.null(badgeStatus)) shinydashboard:::validateStatus(badgeStatus)
-  items <- c(list(...), .list)
-  lapply(items, shinydashboard:::tagAssert, type = "li")
-  dropdownClass <- paste0("dropdown ", type, "-menu")
-  if (is.null(icon)) {
-    icon <- switch(type, messages = shiny::icon("envelope"), 
-                   notifications = shiny::icon("warning"), tasks = shiny::icon("tasks"))
-  }
-  numItems <- length(items)
-  if (is.null(badgeStatus)) {
-    badge <- NULL
-  }
-  else {
-    badge <- tags$span(class = paste0("label label-", badgeStatus), 
-                       numItems)
-  }
-  tags$li(
-    class = dropdownClass, 
-    a(
-      href = "#", 
-      class = "dropdown-toggle", 
-      `data-toggle` = "dropdown", 
-      icon, 
-      badge
-    ), 
-    tags$ul(
-      class = "dropdown-menu", 
-      tags$li(
-        class = "header", 
-        customSentence(numItems, type)
-      ), 
-      tags$li(
-        tags$ul(class = "menu", items)
-      )
-    )
-  )
-}
 
 ## build ui.R -----------------------------------
 ## 1. header -------------------------------
@@ -96,11 +47,10 @@ siderbar <-
       #style = 'height: 90vh; overflow-y: auto;',
       
       ## 1st tab show the Main dashboard -----------
-      menuItem( "Domestic Violence Counts", tabName = 'dv_dashboard', icon = icon('dashboard'),
-                badgeLabel = maxYear_lb, badgeColor = "blue" ),
+      menuItem( "Domestic Violence Counts", tabName = 'dv_dashboard', icon = icon('dashboard') ),
       
       ## Map with Locations of Help Centers --------------
-      menuItem("Resource Locations Directory", tabName = 'resource_dir', icon = icon('globe') ),
+      menuItem("Resource Locations Directory", tabName = 'resource_dir', icon = icon("map-marker-alt") ),
       
       ## Covid Data and Dog Licenses ----------
       menuItem( "COVID Cases to Dog Licenses", tabName = "covid_cases", icon = icon('barcode'), startExpanded = F),
@@ -114,20 +64,6 @@ siderbar <-
 body <- dashboardBody( 
   ## 3.0. CSS styles in header ----------------------------
   tags$head(
-    # ## JS codes
-    # tags$script(src = "fixedElement.js" ),
-    # tags$style(HTML(".scroller_anchor{height:0px; margin:0; padding:0;}; 
-    #                  .scroller{background: white; 
-    #                   border: 1px solid #CCC; 
-    #                   margin:0 0 10px; 
-    #                   z-index:100; 
-    #                   height:50px; 
-    #                   font-size:18px; 
-    #                   font-weight:bold; 
-    #                   text-align:center; 
-    #                  width:500px;}")),
-    
-    #tags$script(src = "world.js" ),
     tags$script("document.title = 'New Zealand Trade Intelligence Dashboard'"),
     
     ### Styles 
@@ -150,30 +86,23 @@ body <- dashboardBody(
     tags$style(HTML(".fa-check { font-size: 14px; }")),
     tags$style(HTML(".fa-times { font-size: 14px; }")),
     
-    #tags$style(HTML(".fa-twitter { font-size: 10px; color:red;}")),
-    #tags$style(HTML(".fa-facebook { font-size: 10px; color:red;}")),
-    #tags$style(HTML(".fa-google-plus { font-size: 10px; color:red;}")),
-    #tags$style(HTML(".fa-pinterest-p { font-size: 10px; color:red;}")),
-    #tags$style(HTML(".fa-linkedin { font-size: 10px; color:red;}")),
-    #tags$style(HTML(".fa-tumblr { font-size: 10px; color:red;}")),
-    
     ## modify the dashboard's skin color
     tags$style(HTML('
                        /* logo */
                        .skin-blue .main-header .logo {
-                       background-color: #006272;
+                       background-color: #FF7F50;
                        }
                        /* logo when hovered */
                        .skin-blue .main-header .logo:hover {
-                       background-color: #006272;
+                       background-color: #FF7F50;
                        }
                        /* navbar (rest of the header) */
                        .skin-blue .main-header .navbar {
-                       background-color: #006272;
+                       background-color: #FF7F50;
                        }
                        /* active selected tab in the sidebarmenu */
                        .skin-blue .main-sidebar .sidebar .sidebar-menu .active a{
-                       background-color: #006272;
+                       background-color: #FF7F50;
                                  }
                        ')
     ),
@@ -201,7 +130,6 @@ body <- dashboardBody(
     tags$style( HTML(".shiny-output-error:before { visibility: hidden; }") ),
     
     ## heand dropdown menu size
-    #tags$style(HTML('.navbar-custom-menu>.navbar-nav>li>.dropdown-menu { width:100px;}'))
     tags$style(HTML('.navbar-custom-menu>.navbar-nav>li:last-child>.dropdown-menu { width:10px; font-size:10px; padding:1px; margin:1px;}')),
     tags$style(HTML('.navbar-custom-menu> .navbar-nav> li:last-child > .dropdown-menu > h4 {width:0px; font-size:0px; padding:0px; margin:0px;}')),
     tags$style(HTML('.navbar-custom-menu> .navbar-nav> li:last-child > .dropdown-menu > p {width:0px; font-size:0px; padding:0px; margin:0px;}'))
@@ -209,7 +137,9 @@ body <- dashboardBody(
   
   ## 3.1 Dashboard body --------------
   tabItems(
-    ## 3.1 Main dashboard ----------------------------------------------------------
+    #############################################################
+    # Domestic Violence Tab
+    ############################################################
     tabItem( tabName = 'dv_dashboard',
              ## contents for the dashboard tab
              # 1.1 Value  Boxes ---------------------------
@@ -227,14 +157,25 @@ body <- dashboardBody(
              fluidRow( column( width = 6,h4("Domestic Violence Case Reports By Borough", align = 'center'), highchartOutput('DVTotalCountsByYear') ),
                        column( width = 6,h4("Domestic Violence Case Reports by Category", align = 'center'), highchartOutput('DVCountsByCategory') )
              )
-             
-             
-   
-   )
+   ),
+   #############################################################
+   # Resource Directory Tab 
+   ############################################################
+     tabItem(
+       ##
+       tabName = 'resource_dir',
+       # Map 
+       div(class="outer",
+           h3(" Domestic Violence Resources", align="center", style="color:#045a8d"),
+           # map output
+           leafletOutput("res_map", width="100%", height=620),
+       )
+       
+     ) 
  )
+
 )
-# 
-# 
+
 
 ## put UI together --------------------
 ui <- 
